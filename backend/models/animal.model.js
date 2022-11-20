8500c70da67dc0889c8aa040a13e64bd55816c5d
Mazen Omar <b9042121@my.shu.ac.uk>
2022-11-20 17:04:41 +0000

    Making Dummy Data

--- backend/models/animal.model.js
+++ backend/models/animal.model.js
@@ -1,13 +1,13 @@
-module.exports = mongoose => {
-    var Animal = mongoose.model(
-        "animal",
-        mongoose.Schema({
-            name: String,
-            species: String,
-            breed: String,
-            age: Number,
-            colour: String
-        })
-    );
-    return Animal;
-};
+// module.exports = mongoose => {
+//     var Animal = mongoose.model(
+//         "animal",
+//         mongoose.Schema({
+//             name: String,
+//             species: String,
+//             breed: String,
+//             age: Number,
+//             colour: String
+//         })
+//     );
+//     return Animal;
+// };
