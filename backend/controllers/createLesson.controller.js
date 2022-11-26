97dd4e15387ac57e944e3685f6fa5e5dd009ce73
Mazen Omar <b9042121@my.shu.ac.uk>
2022-11-26 12:01:42 +0000

    Removed extra thing

--- backend/controllers/createLesson.controller.js
+++ backend/controllers/createLesson.controller.js
@@ -6,53 +6,52 @@ const Attendance = db.attendances;
 
 // Find all Attendance. 
 exports.create = (req, res) => {
-        if (!req.body) {
-            res.status(400).send({ message: "Content can not be empty!" });
-            return;
-        }
-        group = req.params.groupid;
-        lessonDate = req.params.date;
-        console.log(lessonDate)
-        console.log(req.params.time)
+    if (!req.body) {
+        res.status(400).send({ message: "Content can not be empty!" });
+        return;
+    }
+    group = req.params.groupid;
+    lessonDate = req.params.date;
+    console.log(lessonDate)
+    console.log(req.params.time)
 
-        Group.find({ _id: group }, function(groupErrs, updatedGroup) {
-                    if (groupErrs) {
-                        Group.find({ _id: group }, function(groupErrs, updatedGroups) {
-                            if (groupErrs) {
-                                console.log(groupErrs);
-                                res.status(500).send({
-                                    message: groupErrs || "Some error occurred while creating the Lesson."
-                                });
-                                res.status(500).send({
-                                    message: groupErrs || "Some error occurred while creating the Lesson."
-                                });
-                            } else {
-                                updatedGroup = updatedGroups[0];
-                                Lesson.create({ date: lessonDate }, function(lessonErrs, newLesson) {
-                                    if (lessonErrs) {
-                                        console.log(lessonErrs);
+    Group.find({ _id: group }, function(groupErrs, updatedGroup) {
+        if (groupErrs) {
+            Group.find({ _id: group }, function(groupErrs, updatedGroups) {
+                if (groupErrs) {
+                    console.log(groupErrs);
+                    res.status(500).send({
+                        message: groupErrs || "Some error occurred while creating the Lesson."
+                    });
+                } else {
+                    updatedGroup = updatedGroups[0];
+                    Lesson.create({ date: lessonDate }, function(lessonErrs, newLesson) {
+                        if (lessonErrs) {
+                            console.log(lessonErrs);
+                            res.status(500).send({
+                                message: lessonErrs || "Some error occurred while creating the Lesson."
+                            });
+                        } else {
+                            Group.findByIdAndUpdate(group, { $push: { lessons: newLesson } }, { new: true, useFindAndModify: false }).catch(err2 => console.log(err2));
+                            for (let i = 0; i < updatedGroup.students.length; i++) {
+                                Attendance.create({ student: updatedGroup.students[i] }, function(attenErr, attenCreated) {
+                                    if (attenErr) {
+                                        console.log(attenErr);
                                         res.status(500).send({
-                                            message: lessonErrs || "Some error occurred while creating the Lesson."
+                                            message: attenErr || "Some error occurred while creating the attendance."
                                         });
                                     } else {
-                                        Group.findByIdAndUpdate(group, { $push: { lessons: newLesson } }, { new: true, useFindAndModify: false }).catch(err2 => console.log(err2));
-                                        for (let i = 0; i < updatedGroup.students.length; i++) {
-                                            Attendance.create({ student: updatedGroup.students[i] }, function(attenErr, attenCreated) {
-                                                if (attenErr) {
-                                                    console.log(attenErr);
-                                                    res.status(500).send({
-                                                        message: attenErr || "Some error occurred while creating the attendance."
-                                                    });
-                                                } else {
-                                                    Lesson.findByIdAndUpdate(newLesson, { $push: { attendance: attenCreated } }, { new: true, useFindAndModify: false })
-                                                        .catch(err2 => console.log(err2));
-                                                }
-                                            })
-                                        }
-                                        //console.log(newLesson._id)
-                                        res.status(200).send({ lessonid: newLesson._id });
+                                        Lesson.findByIdAndUpdate(newLesson, { $push: { attendance: attenCreated } }, { new: true, useFindAndModify: false })
+                                            .catch(err2 => console.log(err2));
                                     }
                                 })
                             }
-                        })
-                    }
+                            //console.log(newLesson._id)
+                            res.status(200).send({ lessonid: newLesson._id });
+                        }
+                    })
+                }
+            })
+        }
+    })
+}
